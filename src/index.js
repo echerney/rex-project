3a201ca11563a6480fbde001b60f025f749af009
Erin <erin.cherney@wework.com>
2019-05-11 14:41:35 -0400

    refactored rendering function

--- src/index.js
+++ src/index.js
@@ -11,361 +11,195 @@ import 'd3-transition'
 import './style.css'
 import data from '../data/memex-data.csv'
 
-let roomsData = data.filter(function(d) { return d.numrooms > 0 })
-let feedData = data.filter(function(d) { return d.numfeed > 0 })
-let supportData = data.filter(function(d) { return d.numsupport > 0 })
+let journeyVal
+let numJourneyVal
+let completedVal
+let margins
+let fullWidth
+let fullHeight
+let height
+let width
+let filteredData
 
-
-
-document.getElementById("journey-filter").addEventListener("change", function(e){
-  let val = document.getElementById("journey-filter").value
-  console.log(val)
-
-  /*
-  ROOMS JOURNEY
-  */
-
-  if (val == 'rooms') {
-    const sessions = roomsData.map(d => d.session_uuid)
-    const steps = data.map(d => d.t)
-    const sessionsScaleYAxis = scaleBand().domain(sessions).range([roomsData.length * 5, 0])
-    const stepsScaleXAxis = scaleBand().domain(steps).range([0, width])
-    const events = ['app_home', 'rooms_home', 'rooms_scroll', 'rooms_detail', 'rooms_reservation', 'feed_home', 'feed_scroll', 'feed_interact', 'support_home', 'support_detail', 'support_submit']
-    const eventColors = ['#ffffff', '#e8f4f7', '#a3d4e2', '#5eb4cd', '#0d4a5c', '#f7ebe8', '#e2b1a3', '#bf5130', '#e8f7eb', '#a3e2b1', '#0d5c1f']
-    const success = ['transition', 'success']
-    const successColors =  ['#000000', 'gold']
-
-
-    const coloredByEventScale = scaleOrdinal()
-      .domain(events)
-      .range(eventColors)
-
-    const coloredBySuccessScale = scaleOrdinal()
-      .domain(success)
-      .range(successColors)
-
-    /*
-    BUBBLES
-    */
-    d3.select("svg").remove()
-
-    const svg = select('body')
-      .append('svg')
-      .attr('height', height + margins.top + margins.bottom)
-      .attr('width', width + margins.left + margins.right)
-      .append('g')
-      .attr('transform', `translate(${margins.left},${margins.top})`)
-
-    const xAxis = axisBottom(stepsScaleXAxis)
-    const yAxis = axisLeft(sessionsScaleYAxis)
-
-
-
-    const circles = svg.selectAll('circle')
-      .data(roomsData)
-      .enter()
-      .append('circle')
-      .attr('cx',function (d) { return stepsScaleXAxis(d.t) })
-      .attr('cy',function (d) { return sessionsScaleYAxis(d.session_uuid) })
-      .attr('r','10')
-      .attr('stroke',d => coloredBySuccessScale(d.event_type))
-      .attr('stroke-width',1)
-      .attr('fill', d => coloredByEventScale(d.event_name))
-      .on('mouseover', d => {
-        const x = event.pageX
-        const y = window.innerHeight - event.pageY
-        makeTooltipConstructor(x, y, d)
-      })
-      .on('mouseout', () => {
-        select('#tooltip')
-        .style('opacity', 0)
-      })
-  } else if (val == 'support') {
-
-    /*
-    SUPPORT JOURNEY
-    */
-
-    const sessions = supportData.map(d => d.session_uuid)
-    const steps = data.map(d => d.t)
-    const sessionsScaleYAxis = scaleBand().domain(sessions).range([supportData.length * 9, 0])
-    const stepsScaleXAxis = scaleBand().domain(steps).range([0, width])
-    const events = ['app_home', 'rooms_home', 'rooms_scroll', 'rooms_detail', 'rooms_reservation', 'feed_home', 'feed_scroll', 'feed_interact', 'support_home', 'support_detail', 'support_submit']
-    const eventColors = ['#ffffff', '#e8f4f7', '#a3d4e2', '#5eb4cd', '#0d4a5c', '#f7ebe8', '#e2b1a3', '#bf5130', '#e8f7eb', '#a3e2b1', '#0d5c1f']
-    const success = ['transition', 'success']
-    const successColors =  ['#000000', 'gold']
-
-
-    const coloredByEventScale = scaleOrdinal()
-      .domain(events)
-      .range(eventColors)
-
-    const coloredBySuccessScale = scaleOrdinal()
-      .domain(success)
-      .range(successColors)
-
-
-    /*
-    BUBBLES
-    */
-    d3.select("svg").remove()
-
-    const svg = select('body')
-      .append('svg')
-      .attr('height', height + margins.top + margins.bottom)
-      .attr('width', width + margins.left + margins.right)
-      .append('g')
-      .attr('transform', `translate(${margins.left},${margins.top})`)
-
-    const xAxis = axisBottom(stepsScaleXAxis)
-    const yAxis = axisLeft(sessionsScaleYAxis)
-
-
-
-    const circles = svg.selectAll('circle')
-      .data(supportData)
-      .enter()
-      .append('circle')
-      .attr('cx',function (d) { return stepsScaleXAxis(d.t) })
-      .attr('cy',function (d) { return sessionsScaleYAxis(d.session_uuid) })
-      .attr('r','10')
-      .attr('stroke',d => coloredBySuccessScale(d.event_type))
-      .attr('stroke-width',1)
-      .attr('fill', d => coloredByEventScale(d.event_name))
-      .on('mouseover', d => {
-        const x = event.pageX
-        const y = window.innerHeight - event.pageY
-        makeTooltipConstructor(x, y, d)
-      })
-      .on('mouseout', () => {
-        select('#tooltip')
-        .style('opacity', 0)
-      })
-  } else if (val == 'feed') {
-
-    /*
-    FEED JOURNEY
-    */
-
-    const sessions = feedData.map(d => d.session_uuid)
-    const steps = data.map(d => d.t)
-    const sessionsScaleYAxis = scaleBand().domain(sessions).range([feedData.length * 6, 0])
-    const stepsScaleXAxis = scaleBand().domain(steps).range([0, width])
-    const events = ['app_home', 'rooms_home', 'rooms_scroll', 'rooms_detail', 'rooms_reservation', 'feed_home', 'feed_scroll', 'feed_interact', 'support_home', 'support_detail', 'support_submit']
-    const eventColors = ['#ffffff', '#e8f4f7', '#a3d4e2', '#5eb4cd', '#0d4a5c', '#f7ebe8', '#e2b1a3', '#bf5130', '#e8f7eb', '#a3e2b1', '#0d5c1f']
-    const success = ['transition', 'success']
-    const successColors =  ['#000000', 'gold']
-
-
-    const coloredByEventScale = scaleOrdinal()
-      .domain(events)
-      .range(eventColors)
-
-    const coloredBySuccessScale = scaleOrdinal()
-      .domain(success)
-      .range(successColors)
-
-
-    /*
-    BUBBLES
-    */
-    d3.select("svg").remove()
-
-    const svg = select('body')
-      .append('svg')
-      .attr('height', height + margins.top + margins.bottom)
-      .attr('width', width + margins.left + margins.right)
-      .append('g')
-      .attr('transform', `translate(${margins.left},${margins.top})`)
-
-    const xAxis = axisBottom(stepsScaleXAxis)
-    const yAxis = axisLeft(sessionsScaleYAxis)
-
-
-
-    const circles = svg.selectAll('circle')
-      .data(feedData)
-      .enter()
-      .append('circle')
-      .attr('cx',function (d) { return stepsScaleXAxis(d.t) })
-      .attr('cy',function (d) { return sessionsScaleYAxis(d.session_uuid) })
-      .attr('r','10')
-      .attr('stroke',d => coloredBySuccessScale(d.event_type))
-      .attr('stroke-width',1)
-      .attr('fill', d => coloredByEventScale(d.event_name))
-      .on('mouseover', d => {
-        const x = event.pageX
-        const y = window.innerHeight - event.pageY
-        makeTooltipConstructor(x, y, d)
-      })
-      .on('mouseout', () => {
-        select('#tooltip')
-        .style('opacity', 0)
-      })
-  } else if (val == 'none') {
-    const sessions = data.map(d => d.session_uuid)
-    const steps = data.map(d => d.t)
-    const sessionsScaleYAxis = scaleBand().domain(sessions).range([data.length * 5.5, 0])
-    const stepsScaleXAxis = scaleBand().domain(steps).range([0, width])
-    const events = ['app_home', 'rooms_home', 'rooms_scroll', 'rooms_detail', 'rooms_reservation', 'feed_home', 'feed_scroll', 'feed_interact', 'support_home', 'support_detail', 'support_submit']
-    const eventColors = ['#ffffff', '#e8f4f7', '#a3d4e2', '#5eb4cd', '#0d4a5c', '#f7ebe8', '#e2b1a3', '#bf5130', '#e8f7eb', '#a3e2b1', '#0d5c1f']
-    const success = ['transition', 'success']
-    const successColors =  ['#000000', 'gold']
-
-
-    const coloredByEventScale = scaleOrdinal()
-      .domain(events)
-      .range(eventColors)
-
-    const coloredBySuccessScale = scaleOrdinal()
-      .domain(success)
-      .range(successColors)
-
-
-    /*
-    BUBBLES
-    */
-    d3.select("svg").remove()
-
-    const svg = select('body')
-      .append('svg')
-      .attr('height', height + margins.top + margins.bottom)
-      .attr('width', width + margins.left + margins.right)
-      .append('g')
-      .attr('transform', `translate(${margins.left},${margins.top})`)
-
-    const xAxis = axisBottom(stepsScaleXAxis)
-    const yAxis = axisLeft(sessionsScaleYAxis)
-
-
-
-    const circles = svg.selectAll('circle')
-      .data(data)
-      .enter()
-      .append('circle')
-      .attr('cx',function (d) { return stepsScaleXAxis(d.t) })
-      .attr('cy',function (d) { return sessionsScaleYAxis(d.session_uuid) })
-      .attr('r','10')
-      .attr('stroke',d => coloredBySuccessScale(d.event_type))
-      .attr('stroke-width',1)
-      .attr('fill', d => coloredByEventScale(d.event_name))
-      .on('mouseover', d => {
-        const x = event.pageX
-        const y = window.innerHeight - event.pageY
-        makeTooltipConstructor(x, y, d)
-      })
-      .on('mouseout', () => {
-        select('#tooltip')
-        .style('opacity', 0)
-      })
-  }
-
-
-});
+buildDropdowns()
+renderData(journeyVal,numJourneyVal,completedVal)
 
 /*
 BUILD FILTERS
 */
 
-
-function fillDropdowns() {
-  let numJourneys = roomsData.map(d => d.numjourneys)
+function buildDropdowns() {
+  let numJourneys = data.map(d => d.numjourneys)
   let numCompleted = data.map(d => d.numsuccess)
   let maxNumJourneys = Math.max.apply(0,numJourneys)
   let maxNumCompleted = Math.max.apply(0,numCompleted)
   let numberSelect = select('#num-journey-filter')
   let completedSelect = select('#completed-filter')
-
   for (let i=0;i<maxNumJourneys;i++) {
     numberSelect.append('option')
     .text(i+1)
   }
-
   for (let i=0;i<=maxNumCompleted;i++) {
     completedSelect.append('option')
     .text(i)
   }
+}
+
+/*
+EVENT LISTENERS
+*/
+
+document.getElementById("journey-filter").addEventListener("change", function(e){
+  let journeyVal = document.getElementById("journey-filter").value
+  let numJourneyVal = document.getElementById("num-journey-filter").value
+  let completedVal = document.getElementById("completed-filter").value
+  console.log(journeyVal, numJourneyVal,completedVal)
+  renderData(journeyVal,numJourneyVal,completedVal)
+})
+
+document.getElementById("num-journey-filter").addEventListener("change", function(e){
+  let journeyVal = document.getElementById("journey-filter").value
+  let numJourneyVal = document.getElementById("num-journey-filter").value
+  let completedVal = document.getElementById("completed-filter").value
+  console.log(journeyVal, numJourneyVal,completedVal)
+    renderData(journeyVal,numJourneyVal,completedVal)
+})
+
+document.getElementById("completed-filter").addEventListener("change", function(e){
+  let journeyVal = document.getElementById("journey-filter").value
+  let numJourneyVal = document.getElementById("num-journey-filter").value
+  let completedVal = document.getElementById("completed-filter").value
+  console.log(journeyVal, numJourneyVal,completedVal)
+    renderData(journeyVal,numJourneyVal,completedVal)
+})
+
+function renderData(journeyVal,numJourneyVal,completedVal){
+
+  //FILTERING DATA BY JOURNEY
+  let stepOneData = data
+  if (journeyVal) {
+    let filterByJourney = function (journeyVal){
+      if (journeyVal == 'rooms') {
+        stepOneData = data.filter(function(d) { return d.numrooms > 0 })
+        console.log("this runs")
+      } else if (journeyVal == 'feed') {
+        stepOneData = data.filter(function(d) { return d.numfeed > 0 })
+      } else if (journeyVal == 'support') {
+        stepOneData = data.filter(function(d) { return d.numsupport > 0 })
+      } else {
+        stepOneData = data
+      }
+    }
+    filterByJourney(journeyVal)
+    console.log("step one", stepOneData)
+  }
+  let stepTwoData = stepOneData
+  //FILTERING DATA BY NUMBER OF JOURNEYS
+  if (numJourneyVal) {
+    let filterByNumJourneys = function(numJourneyVal) {
+      stepTwoData = stepOneData.filter(function(d) { return d.numjourneys >= numJourneyVal })
+    }
+    filterByNumJourneys(numJourneyVal)
+  }
+  let stepThreeData = stepTwoData
+  //FILTERING DATA BY NUMBER OF JOURNEYS THAT ACHIEVED SUCCESS
+  console.log(completedVal)
+  if (completedVal) {
+    let filterByCompleted = function(completedVal) {
+      stepThreeData = stepTwoData.filter(function(d) { return d.numsuccess >= completedVal })
+    }
+    filterByCompleted(completedVal)
+  }
+  filteredData = stepThreeData
+  console.log("filtered: ",filteredData)
+
+
+
+  // VARIABLES FOR SCALES
+
+  const sessions = filteredData.map(d => d.session_uuid)
+  const steps = filteredData.map(d => d.t)
+  const events = ['app_home', 'rooms_home', 'rooms_scroll', 'rooms_detail', 'rooms_reservation', 'feed_home', 'feed_scroll', 'feed_interact', 'support_home', 'support_detail', 'support_submit']
+  const eventColors = ['#ffffff', '#e8f4f7', '#a3d4e2', '#5eb4cd', '#0d4a5c', '#f7ebe8', '#e2b1a3', '#bf5130', '#e8f7eb', '#a3e2b1', '#0d5c1f']
+  const success = ['transition', 'success']
+  const successColors =  ['#000000', 'gold']
+
+  // CANVAS SIZE
+
+  margins = { left: 100, top: 50, right: 50, bottom: 100 }
+  fullWidth = +select('body').node().getBoundingClientRect().width
+  fullHeight = +select('body').node().getBoundingClientRect().height
+  width = fullWidth * .7
+  if (filteredData.length < 40) {
+    height = filteredData.length * 9
+  } else {
+    height = filteredData.length * 5.5
+  }
+
+    // BUILD SCALES
+
+  const sessionsScaleYAxis = scaleBand()
+    .domain(sessions)
+    .range([height, 0])
+  const stepsScaleXAxis = scaleBand()
+    .domain(steps)
+    .range([0, width])
+
+  const coloredByEventScale = scaleOrdinal()
+    .domain(events)
+    .range(eventColors)
+
+  const coloredBySuccessScale = scaleOrdinal()
+    .domain(success)
+    .range(successColors)
+
+    /*
+    BUBBLES
+    */
+
+    d3.select("svg").remove()
+
+    const svg = select('body')
+      .append('svg')
+      .attr('height', height + margins.top + margins.bottom)
+      .attr('width', width + margins.left + margins.right)
+      .append('g')
+      .attr('transform', `translate(${margins.left},${margins.top})`)
+
+    const xAxis = axisBottom(stepsScaleXAxis)
+    const yAxis = axisLeft(sessionsScaleYAxis)
+
+
+
+    const circles = svg.selectAll('circle')
+      .data(filteredData)
+      .enter()
+      .append('circle')
+      .attr('cx',function (d) { return stepsScaleXAxis(d.t) })
+      .attr('cy',function (d) { return sessionsScaleYAxis(d.session_uuid) })
+      .attr('r','10')
+      .attr('stroke',d => coloredBySuccessScale(d.event_type))
+      .attr('stroke-width',1)
+      .attr('fill', d => coloredByEventScale(d.event_name))
+      .on('mouseover', d => {
+        const x = event.pageX
+        const y = window.innerHeight - event.pageY
+        makeTooltipConstructor(x, y, d)
+      })
+      .on('mouseout', () => {
+        select('#tooltip')
+        .style('opacity', 0)
+      })
+
+
 
 }
 
-fillDropdowns()
-
-/*
-CANVAS SIZE
-*/
-
-
-const margins = { left: 100, top: 50, right: 50, bottom: 100 }
-
-const fullWidth = +select('body').node().getBoundingClientRect().width
-const fullHeight = +select('body').node().getBoundingClientRect().height
-
-const height = (fullHeight * 6.5) - margins.top - margins.bottom
-const width = (fullWidth * .85) - margins.left - margins.right
-
-
-/*
-SCALES
-*/
-
-const sessions = data.map(d => d.session_uuid)
-const steps = data.map(d => d.t)
-const events = ['app_home', 'rooms_home', 'rooms_scroll', 'rooms_detail', 'rooms_reservation', 'feed_home', 'feed_scroll', 'feed_interact', 'support_home', 'support_detail', 'support_submit']
-const eventColors = ['#ffffff', '#e8f4f7', '#a3d4e2', '#5eb4cd', '#0d4a5c', '#f7ebe8', '#e2b1a3', '#bf5130', '#e8f7eb', '#a3e2b1', '#0d5c1f']
-const success = ['transition', 'success']
-const successColors =  ['#000000', 'gold']
-
-const sessionsScaleYAxis = scaleBand()
-  .domain(sessions)
-  .range([data.length * 5.5, 0])
-const stepsScaleXAxis = scaleBand()
-  .domain(steps)
-  .range([0, width])
-
-const coloredByEventScale = scaleOrdinal()
-  .domain(events)
-  .range(eventColors)
-
-const coloredBySuccessScale = scaleOrdinal()
-  .domain(success)
-  .range(successColors)
-
-
-/*
-BUBBLES
-*/
-
-const svg = select('body')
-  .append('svg')
-  .attr('height', height + margins.top + margins.bottom)
-  .attr('width', width + margins.left + margins.right)
-  .append('g')
-  .attr('transform', `translate(${margins.left},${margins.top})`)
-
-const xAxis = axisBottom(stepsScaleXAxis)
-const yAxis = axisLeft(sessionsScaleYAxis)
-
-
-
-const circles = svg.selectAll('circle')
-  .data(data)
-  .enter()
-  .append('circle')
-  .attr('cx',function (d) { return stepsScaleXAxis(d.t) })
-  .attr('cy',function (d) { return sessionsScaleYAxis(d.session_uuid) })
-  .attr('r','10')
-  .attr('stroke',d => coloredBySuccessScale(d.event_type))
-  .attr('stroke-width',1)
-  .attr('fill', d => coloredByEventScale(d.event_name))
-  .on('mouseover', d => {
-    const x = event.pageX
-    const y = window.innerHeight - event.pageY
-    makeTooltipConstructor(x, y, d)
-  })
-  .on('mouseout', () => {
-    select('#tooltip')
-    .style('opacity', 0)
-  })
+margins = { left: 100, top: 50, right: 50, bottom: 100 }
+fullWidth = +select('body').node().getBoundingClientRect().width
+fullHeight = +select('body').node().getBoundingClientRect().height
+height = 500
+width = 400
 
 
 
